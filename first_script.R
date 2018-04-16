9bc315dea64492a4291baa660db4012e2a9ca9c7
Javier Andres Beltran Valdebenito <n9675230@qut.edu.au>
2018-04-16 20:44:32 +1000

    last first commit

--- first_script.R
+++ first_script.R
@@ -2,3 +2,23 @@
 
 library(dplyr)
 library(lubridate)
+
+# Read datasets available in R
+
+data()
+
+# Load one of the datasets
+
+data(USPersonalExpenditure)
+data(swiss)
+data(uspop)
+
+# see one of the datasets
+
+USPersonalExpenditure
+uspop
+
+# using Pacrat
+# Were are my packages?
+
+.libPaths()
